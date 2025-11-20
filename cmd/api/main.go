6cf95fdbdf661f125d68cbde906416740c211326
sinaniya <sinaniya@gmail.com>
2025-11-20 13:13:51 +0100

    defined todo service

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/snnyvrz/go-todo-crud-nethttp/internal/todos"
+)
+
+func main() {
+	todoService := todos.NewService()
+
+	mux := http.NewServeMux()
+
+	log.Println("Server listening on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
--- internal/todos/service.go
+++ internal/todos/service.go
@@ -1,1 +1,16 @@
 package todos
+
+import "sync"
+
+type Service struct {
+	mu     sync.Mutex
+	todos  map[int]Todo
+	nextID int
+}
+
+func NewService() *Service {
+	return &Service{
+		todos:  make(map[int]Todo),
+		nextID: 1,
+	}
+}
