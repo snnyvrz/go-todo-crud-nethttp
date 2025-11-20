bdf41c13d309843cff42c6552f2e1a151a02c6d9
sinaniya <sinaniya@gmail.com>
2025-11-20 13:53:06 +0100

    implemented get todos

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/snnyvrz/go-todo-crud-nethttp/internal/todos"
 )
 
+type application struct {
+	todoService *todos.Service
+}
+
+func (app *application) listTodoHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	todos, err := app.todoService.List()
+
+	if err != nil {
+		http.Error(w, "failed to list todos", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
+		http.Error(w, "failed to encode response", http.StatusInternalServerError)
+		return
+	}
+}
+
 func main() {
-	todoService := todos.NewService()
+	app := &application{
+		todoService: todos.NewService(),
+	}
 
 	mux := http.NewServeMux()
+	mux.HandleFunc("/todos", app.listTodoHandler)
 
 	log.Println("Server listening on :8080")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
